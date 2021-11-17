d2ae93020b7531237554d24914f7a87d1ea58f6f
Michael Bodin <92333134+zenandtonic@users.noreply.github.com>
2021-11-17 11:59:50 -0600

    first push

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+})
+
+
+
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log(`My app is singing on port number ${port}`);
+});
